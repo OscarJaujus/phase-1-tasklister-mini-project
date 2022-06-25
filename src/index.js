63442198901b60adf9cbed2a4b0ed45694130bf7
Oscar Omondi <oscaromondiomondi@gmail.com>
2022-06-25 21:10:19 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      handleToDo(event.target.newtask.value)
+      form.reset()
+  })
+
+})
+
+function handleToDo(todo) {
+
+  let task = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', (event) =>
+      event.target.parentNode.remove()
+
+  )
+
+  btn.textContent = "  x"
+
+  task.textContent = `   ${todo}`
+
+  task.appendChild(btn)
+
+  document.querySelector('#tasks').appendChild(task)
+
+}
